d1c7abd0b1d94c0bf87c8d6eab8388d5abc198d1
NavonilDas <navneeladas@gmail.com>
2018-07-25 15:53:11 +0530

    Splash Screen Implemented

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace BatchWatermark
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Splash());
         }
+
     }
 }
